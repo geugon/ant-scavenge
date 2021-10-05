e9f33aa9c70fddd3bd9248ee7bf6d29c10e0526d
geugon <geugon@gmail.com>
2021-10-05 13:53:15 -0700

    add SpriteManager

--- ant-scavenge/visualizer.py
+++ ant-scavenge/visualizer.py
@@ -9,21 +9,87 @@ import numpy as np
 
 colors = {'white-ish':  (230,230,230),
           'black':      (0,0,0),
-          'dark grey':  (60,60,60)}
+          'dark grey':  (60,60,60),
+          'red':        (255,0,0),
+          'dark red':   (128,0,0),
+          'green':      (0,255,0),
+          }
 
 
-class Wall(pygame.sprite.Sprite):
+class Piece(pygame.sprite.Sprite):
     def __init__(self, size, pos, imgFile=None):
         super().__init__()
-        
+
         if imgFile:
             self.image = pygame.image.load(imgFile).convert_alpha()
         else:
             self.image = pygame.Surface(size)
-            self.image.fill(colors['dark grey'])
-
+            self.image.fill(self._color)
+ 
         self.rect = self.image.get_rect()
         self.rect = pos
+
+
+class Wall(Piece):
+    _color = colors['dark grey']
+
+
+class Mound(Piece):
+    _color = colors['dark red']
+
+
+class Ant(Piece):
+    _color = colors['red']
+
+
+class Food(Piece):
+    _color = colors['green']
+
+
+class SpriteManager():
+    """
+    Builds and Stores Sprites
+    """
+    _class_lookup = {'walls': Wall,
+                     'ants': Ant,
+                     'food': Food,
+                     'mound': Mound,
+                    }
+
+    def __init__(self, _voxel_size, _boarder_size):
+        self._voxel_size = _voxel_size
+        self._boarder_size = _boarder_size
+        self.piece_size = Point(self._voxel_size-self._boarder_size, self._voxel_size-self._boarder_size)
+
+        self.sprites = {'walls': pygame.sprite.Group(),
+                        'ants': pygame.sprite.Group(),
+                        'mound': pygame.sprite.Group(),
+                        'food': pygame.sprite.Group(),
+                       }
+        self.src_data = {'walls': np.array(0),
+                        'ants': np.array(0),
+                        'mound': np.array(0),
+                        'food': np.array(0),
+                       }
+
+    def update(self, data):
+        for item in data.items():
+            self._build_group(*item)
+
+    def get_all(self):
+        return self.sprites
+
+    def _build_group(self, name, data):
+        if np.array_equal(self.src_data[name], data):
+            return #no change
+
+        group = pygame.sprite.Group()
+        for coord in np.asarray(np.where(data)).T:
+            pos = Point.cast(coord)*self._voxel_size + self._boarder_size
+            group.add(self._class_lookup[name](self.piece_size, pos))
+
+        self.sprites[name] = group
+        self.src_data[name] = data
 
 
 class Visualizer():
@@ -36,12 +102,13 @@ class Visualizer():
         # Sizes of board area, not full image
         self.x_size = self._boarder_size + shape.x*self._voxel_size
         self.y_size = self._boarder_size + shape.y*self._voxel_size 
-        self.piece_size = Point(self._voxel_size-self._boarder_size, self._voxel_size-self._boarder_size)
 
         pygame.init()
         self.screen = pygame.display.set_mode((self.x_size + self._right_space, self.y_size))
         self.background = self._build_background()
         pygame.display.set_caption("ant-scavenge")
+
+        self.sprites = SpriteManager(self._voxel_size, self._boarder_size)
 
     def _build_background(self):
         background = pygame.Surface(self.screen.get_size())
@@ -61,12 +128,9 @@ class Visualizer():
     def show(self, board):
         self.screen.blit(self.background, (0,0))
 
-        walls = pygame.sprite.Group()
-        for wall_coord in np.asarray(np.where(board.data['walls'])).T:
-            pos = Point.cast(wall_coord)
-            pos = Point.cast(wall_coord)*self._voxel_size + self._boarder_size
-            walls.add(Wall(self.piece_size, pos))
-        walls.draw(self.screen)
+        self.sprites.update(board.data)
+        for name, group in self.sprites.get_all().items():
+            group.draw(self.screen)
  
         pygame.display.update()
         pygame.time.delay(4000)
@@ -83,5 +147,6 @@ if __name__=="__main__":
 
     vis = Visualizer(board1.shape)
     vis.show(board1)
+    vis.show(board2)
     vis.show(board2)
     vis.close()
